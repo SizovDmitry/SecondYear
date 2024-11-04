4cc6c7d6060bc4eeb9fe481f3b90bb9e7f04be33
SizovDmitry <184sizov9v@mail.ru>
2024-11-04 18:57:32 +0300

    Work done

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,32 +1,73 @@
+#include <iostream>
 
-template<typename T>
-class ArrayHandler {
-private:
-    size_t _size;
-    T* _array;
-public:
-    ArrayHandler(size_t size = 10) {
+using namespace std;
 
-    }
+template <typename T>
 
-    void AppendElem(T elem) {
+class ArrayHandler
+{
 
-    }
+    private:
+        size_t size;
+        size_t _size;
+        T* arr;
 
-    bool IsContains(T elem) {
-        return false;
-    }
 
-    T GetMax() {
-        return 0;
-    }
+    public:
 
-    T GetMin() {
-        return 0;
-    }
+        ArrayHandler() {
+            size = 0;
+            _size = 0;
+            arr = new T[_size];
+        };
 
-    ~ArrayHandler() {
+        void AppendElem(T elem) {
+            if (size == _size) {
+                if (_size == 0) {
+                    _size = 1;
+                } else {
+                    _size = _size * 2;
+                }
+                T* new_arr = new T[_size];
+                for (size_t i = 0; i < size; i++) {
+                    new_arr[i] = arr[i];
+                };
+                delete []arr;
+                arr = new_arr;
+            };
 
-    }
+            arr[size] = elem;
+            size++;                
+        };
+        
+        T GetMax() {
+            T max = arr[0];
+            for (size_t i = 0; i < size; i++) {
+                if (max < arr[i])
+                    max = arr[i];
+            };
+            return max;
+        };
 
+        T GetMin() {
+            T min = arr[0];
+            for (size_t i = 0; i < size; i++) {
+                if (arr[i] < min)
+                    min = arr[i];
+            };
+            return min;
+        };
+
+        int FindElem(T elem) {
+            for (size_t i = 0; i < size; i++) {
+                if (arr[i] == elem)
+                    return i;
+            };
+            return -1;
+        };
+
+        ~ArrayHandler(){
+            delete []arr;
+        };
+        
 };
--- BeginTask-0/main.cpp
+++ BeginTask-0/main.cpp
@@ -1,10 +1,10 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-#include "ArrayHandler.h"
+#include "ArrayHandler.hpp"
 
-
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-
-    return RUN_ALL_TESTS();
-}
+int main(){
+    ArrayHandler <double> massive;
+    for (int i = 1; i <= 5; i++) {
+        massive.AppendElem(i);
+    }
+    cout << massive.GetMax() << endl;
+    cout << massive.GetMin() << endl;
+};
